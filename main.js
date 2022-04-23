d94d83474befc50f061aa6ef1d6baa60e98be92b
Dino Marchiori <dinaco1@gmail.com>
2022-04-23 19:26:51 +0100

    implemented kills and scores

--- main.js
+++ main.js
@@ -1,67 +1,57 @@
 window.onload = () => {
-  /*   document.getElementById("start-button").onclick = () => {
-      gameLogic.start();
-    }; */
-
-  /*   document.addEventListener("keydown", (e) => {
-      switch (e.code) {
-        case "ArrowLeft":
-          position.moveLeft();
-          break;
-        case "ArrowRight":
-          position.moveRight();
-          break;
-      }
-    }); */
-
   const canvas = document.getElementById("game-board");
   const ctx = canvas.getContext("2d");
 
   const cWidth = canvas.width;
   const cHeight = canvas.height;
 
-  // setTimeout, setInterval, requestAnimationFrame
+  // BEGIN LISTENERS
 
-  const color = {
-    red: Math.floor(Math.random() * 255),
-    green: Math.floor(Math.random() * 255),
-    blue: Math.floor(Math.random() * 255),
-    rgb: function () {
-      return `rgb(${this.red}, ${this.green}, ${this.blue})`;
-    },
-  };
+  /*   document.getElementById("start-button").onclick = () => {
+      gameLogic.start();
+    }; */
 
-  /* const updateCanvas = () => {
-      color.red = (color.red + 1) % 255;
-      color.green = (color.green + 1) % 255;
-      color.blue = (color.blue + 1) % 255;
-      ctx.clearRect(0, 0, cWidth, cHeight);
-      ctx.fillStyle = color.rgb();
-      ctx.fillRect(100, 100, 250, 250);
-      requestAnimationFrame(updateCanvas);
-    };
-    updateCanvas(); */
+  document.addEventListener("keydown", (e) => {
+    switch (e.code) {
+      case "ArrowUp":
+        newPoke.moveUp();
+        break;
+      case "ArrowDown":
+        newPoke.moveDown();
+        break;
+      case "ArrowLeft":
+        newPoke.moveLeft();
+        break;
+      case "ArrowRight":
+        newPoke.moveRight();
+        break;
+      case "Space":
+        attack();
+        break;
+    }
+  });
 
-  function clearCanvas() {
-    ctx.clearRect(0, 0, cWidth, cHeight);
-  }
+  // END LISTENERS
 
-  // OOP - User inputs
-
-  ctx.font = "18px serif";
-
+  // BEGIN COMPONENTS
   class Pokemon {
     constructor(x, y, url) {
       this.x = x;
       this.y = y;
-
+      this.direction = "";
       const img = new Image();
       this.img = img;
       this.drawCharacter();
       img.src = url;
-      /* 'https://www.seekpng.com/png/full/182-1829970_caterpie-pokemon-sprite-animation-gifs.png' */
+      this.frames = 0;
     }
-
+    attack(x, y) {
+      const attackImg = new Image();
+      this.attackImg = attackImg;
+      this.attackImg.src =
+        "https://www.clipartmax.com/png/full/250-2502429_fireball-clipart-pixel-sprite-pixel-transparent-fire-ball.png";
+      ctx.drawImage(this.attackImg, x, y, 100, 100);
+    }
     moveUp() {
       this.y - 50 >= 0 ? (this.y -= 50) : false;
     }
@@ -71,10 +61,12 @@ window.onload = () => {
     }
 
     moveLeft() {
+      this.direction = "left";
       this.x - 50 >= 0 ? (this.x -= 50) : false;
     }
 
     moveRight() {
+      this.direction = "right";
       this.x + 100 <= cWidth ? (this.x += 50) : false;
     }
 
@@ -84,151 +76,188 @@ window.onload = () => {
     }
 
     drawCharacter() {
-      ctx.drawImage(this.img, this.x, this.y, 50, 50);
+      //  ctx.scale(this.direction === "right" ? -1 : 1, 1);
+      ctx.drawImage(
+        this.img,
+        this.x,
+        // this.direction === "right" ? this.x * -1 : this.x,
+        this.y,
+        100,
+        100
+      );
+      //  if (this.direction === "left" || this.direction === "") {
+      // ctx.drawImage(this.img, this.x, this.y, 100, 100);
+      //    } else {
+      //  ctx.translate(this.x + 100, this.y);
+      //  ctx.scale(-1, 1);
+      //  ctx.drawImage(this.img, this.x, this.y, 100, 100);
+      //ctx.setTransform(1, 0, 0, 1, 0, 0);
+      //ctx.setTransform(1,0,0,-1,0,canvas.height);
+      //  }
+    }
+  }
+  class Enemies extends Pokemon {
+    constructor(x, y, url) {
+      super(x, y, url);
+    }
+    drawCharacter() {
+      ctx.drawImage(this.img, this.x, this.y, 100, 100);
+    }
+    dead(enemies) {
+      if (attackList[0]) {
+        if (
+          attackList[0].x < enemies.x + 50 &&
+          attackList[0].x > enemies.x - 50 &&
+          attackList[0].y < enemies.y + 55 &&
+          attackList[0].y > enemies.y - 55
+        ) {
+          console.log("hit");
+          attackList.pop();
+          return true;
+          /*           return !(
+            this.x < enemies.x - 45 || this.x > enemies.x + enemies.width 
+          ); */
+        }
+      }
     }
   }
 
-  const caterpie = new Pokemon(
-    cWidth / 2 - 50,
-    cHeight / 2 - 50,
-    "https://projectpokemon.org/images/shiny-sprite/caterpie.gif"
-  );
-  const bulbasaur = new Pokemon(
-    1100,
-    700,
-    "https://projectpokemon.org/images/normal-sprite/bulbasaur.gif"
-  );
-
-  document.addEventListener("keydown", (e) => {
-    switch (e.code) {
-      case "ArrowUp":
-        caterpie.moveUp();
-        break;
-      case "ArrowDown":
-        caterpie.moveDown();
-        break;
-      case "ArrowLeft":
-        caterpie.moveLeft();
-        break;
-      case "ArrowRight":
-        caterpie.moveRight();
-        break;
-      case "Space":
-        caterpie.reset();
-        break;
-      case "KeyW":
-        bulbasaur.moveUp();
-        break;
-      case "KeyA":
-        bulbasaur.moveLeft();
-        break;
-      case "KeyS":
-        bulbasaur.moveDown();
-        break;
-      case "KeyD":
-        bulbasaur.moveRight();
-        break;
+  class Attack {
+    constructor(x, y, url) {
+      this.x = x;
+      this.y = y;
+      this.direction = "";
+      const img = new Image();
+      this.img = img;
+      img.src = url;
+      this.frames = 0;
     }
+    drawAttack() {
+      ctx.drawImage(this.img, this.x, this.y, 100, 100);
+    }
+  }
+  // END COMPONENTS
 
-    updateCanvas();
-  });
-
-  const draw = () => {
-    //rectangles
-    /*   ctx.fillStyle = 'purple';
-      ctx.fillRect(125, 125, 300, 300);
-      ctx.clearRect(145, 145, 260, 260);
-      ctx.strokeRect(150, 150, 250, 250);
-      ctx.fillStyle = 'color'
-      ctx.strokeStyle = 'color'
-      ctx.lineWidth = 20
-      //paths
-      ctx.fillStyle = 'black';
-      ctx.lineWidth = 10; */
-    /* ctx.beginPath();
-      //first line
-      ctx.moveTo(50, 50);
-      ctx.lineTo(250, 50);
-      //moves the pen to the end of the first line
-      ctx.moveTo(250, 50);
-      ctx.lineTo(250, 250); */
-    //strokes the line
-    /*  ctx.stroke();
-      ctx.closePath();
-      ctx.beginPath();
-      ctx.moveTo(75, 50);
-      ctx.lineTo(100, 75);
-      ctx.lineTo(100, 25);
-      ctx.fill();
-      ctx.closePath(); */
-    //Arcs
-    //arc(x, y, radius, startAngle, endAngle, anticlockWise)
-    // radians = (Math.PI/180) * degrees
-    /*  ctx.beginPath();
-      ctx.arc(350, 350, 175, 0, Math.PI * 2);
-      ctx.lineWidth = 20;
-      ctx.strokeStyle = 'green';
-      ctx.stroke();
-      ctx.closePath();
-      ctx.beginPath();
-      ctx.arc(350, 350, 75, 0, Math.PI * 2);
-      ctx.fillStyle = 'red';
-      ctx.fill();
-      ctx.closePath(); */
-    /* let grd = ctx.createLinearGradient(0, 0, 800, 0);
-      grd.addColorStop(0, 'red');
-      grd.addColorStop(1, 'blue');
-      ctx.fillStyle = grd;
-      ctx.fillRect(0, 0, 900, 600); */
-    /*  ctx.font = '64px serif';
-      ctx.fillText('Hello world', 10, 100);
-      ctx.strokeText('Hello world', 10, 300); */
-  };
-
-  //draw();
-
-  //Paths
-  //beginPath()
-  //stroke()
-  //fill()
-  //closePath
-
-  //Color
-  //ctx.fillStyle = 'rgba(2, 2, 2, 0.5)'
-  //ctx.strokeStyle = 'red'
-
-  //Images
+  //BEGIN BACKGROUND
 
   const tileImg = new Image();
 
-  /* tileImg.addEventListener('load', () => {
-      tileImg.src = 'images/tile.jpg';
-    }); */
-
   tileImg.src = "/docs/assets/imgs/tile.jpg";
 
-  let tileX = 0;
-  let tileY = 0;
-
-  const drawCanvas = () => {
-    //ctx.clearRect(0, 0, cWidth, cHeight);
-
+  const drawBg = () => {
     let tilePattern = ctx.createPattern(tileImg, "repeat");
     ctx.fillStyle = tilePattern;
     ctx.fillRect(0, 0, cWidth, cHeight);
-
-    ctx.drawImage(tileImg, tileX % cWidth, tileY, 50, 50);
+    ctx.drawImage(tileImg, 0, 0, 50, 50);
   };
+
+  // END BACKGROUND
+
+  //BEGIN IMAGES
+
+  const newPoke = new Pokemon(
+    cWidth / 2 - 50,
+    cHeight / 2 - 50,
+    "http://www.serebii.net/xy/pokemon/006.png"
+  );
+  /*   const bulbasaur = new Pokemon(
+    1100,
+    700,
+    "https://projectpokemon.org/images/normal-sprite/bulbasaur.gif"
+  ); */
+
+  // END IMAGES
+
+  // BEGIN FUNCTIONS
+  const enemies = [];
+  function updateEnemies() {
+    for (let i = 0; i < enemies.length; i++) {
+      enemies[i].drawCharacter();
+    }
+    if (enemies.length < 5) {
+      if (newPoke.frames === 1 || newPoke.frames % 120 === 0) {
+        const randomPoke = Math.floor(Math.random() * 4 + 1);
+        let minX = 0;
+        let maxX = cWidth - 100;
+
+        let x = Math.floor(Math.random() * (maxX - minX) + minX);
+
+        let minY = 0;
+        let maxY = cHeight - 100;
+        let y = Math.floor(Math.random() * (maxY - minY) + minY);
+        enemies.push(
+          new Enemies(
+            x,
+            y,
+            `http://www.serebii.net/xy/pokemon/00${randomPoke}.png`
+          )
+        );
+      }
+    }
+  }
+  const attackList = [];
+  function attack() {
+    for (let i = 0; i < attackList.length; i++) {
+      if (attackList[i].x < 0 || attackList[i].x > cWidth) {
+        attackList.pop();
+        return false;
+      } else {
+        newPoke.direction !== "right"
+          ? (attackList[i].x -= 20)
+          : (attackList[i].x += 20);
+        attackList[i].drawAttack();
+        kill();
+      }
+    }
+
+    if (attackList.length < 1) {
+      attackList.push(
+        new Attack(
+          newPoke.direction !== "right" ? newPoke.x - 100 : newPoke.x,
+          newPoke.y,
+          `https://www.clipartmax.com/png/full/250-2502429_fireball-clipart-pixel-sprite-pixel-transparent-fire-ball.png`
+        )
+      );
+    }
+  }
+
+  function clearCanvas() {
+    ctx.clearRect(0, 0, cWidth, cHeight);
+  }
+  let points = 0;
+  function score(kills) {
+    points += kills;
+
+    (document.getElementById("score").innerHTML = `${points}`), 100, 50;
+  }
+
+  function kill() {
+    enemies.map((el, i) => {
+      if (el.dead(el)) {
+        enemies.splice([i], 1);
+        score(10);
+      }
+    });
+  }
 
   const updateCanvas = () => {
-    // clearCanvas();
-    drawCanvas();
-    ctx.fillText("Caterpie x: " + caterpie.x, 1000, 40);
-    ctx.fillText("Caterpie y: " + caterpie.y, 1000, 100);
-
-    caterpie.drawCharacter();
-    bulbasaur.drawCharacter();
+    clearCanvas();
+    drawBg();
+    newPoke.drawCharacter();
+    newPoke.frames++;
+    updateEnemies();
+    attackList.length > 0 ? attack() : false;
   };
 
-  updateCanvas();
+  const startGame = () => {
+    const intervalId = setInterval(() => {
+      updateCanvas();
+    }, 30);
+  };
+
+  // END FUNCTIONS
+
+  // START FUNCTION
+
+  startGame();
 };
